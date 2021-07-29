256925d6654a6dfb2c7eac81323711398dce1a46
nikshayinexture <87075176+nikshayinexture@users.noreply.github.com>
2021-07-29 21:12:21 +0530

    Add files via upload

--- usermanager/src/main/java/com/user/controller/UserController.java
+++ usermanager/src/main/java/com/user/controller/UserController.java
@@ -2,6 +2,7 @@ package com.user.controller;
 
 import java.security.Principal;
 import java.util.List;
+import java.util.Optional;
 
 import javax.servlet.http.HttpSession;
 
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -101,5 +103,89 @@ public class UserController {
 		
 		return "normal/display_address";
 	}
+	
+	// Handler For Displaying Address Indiviually
+	
+	@RequestMapping("{a_id}/address")
+	public String displayAddressDetails(@PathVariable("a_id") Integer a_id, Model model, Principal principal) {
+		
+		System.out.println("Address ID "+a_id);
+		
+		Optional<Address> addressOptional = this.addressRepository.findById(a_id);
+		Address address = addressOptional.get();
+		
+		//Security Check
+		String userName = principal.getName();
+		User user = this.userRepository.getUserByUserName(userName);
+		
+		if(user.getId()==address.getUser().getId())
+		{
+		model.addAttribute("address", address);
+		model.addAttribute("title", address.getHouse_no());
+		}
+		return "normal/address_details";
+	}
+	
+	//Handler For Deleting Address
+	
+	@GetMapping("/delete/{a_id}")
+	public String deleteAddress(@PathVariable("a_id") Integer a_id, Model model, HttpSession session)
+	{
+		
+		Optional <Address> addressOptional = this.addressRepository.findById(a_id);
+		Address address = addressOptional.get();
+		
+		System.out.println("Address" +address.getA_id());
+		
+		address.setUser(null);
+		
+		this.addressRepository.delete(address);
+		
+		System.out.println("Deleted");
+		session.setAttribute("message", new Message("Address Deleted Successfully", "success"));	
+		
+		return "redirect:/user/display-address";
+	}
+	
+	//Handler For Updating Address Form
+	
+	@PostMapping("/update-address/{a_id}")
+	public String updateAddress(@PathVariable("a_id") Integer a_id, Model model)
+	{
+		
+		model.addAttribute("title", "Update Address");
+		
+		Address address = this.addressRepository.findById(a_id).get();	
+		model.addAttribute("address", address);
+		
+		return "normal/update_address";
+	}
+	
+	//Update Handler
+	
+	@PostMapping("/process-update")
+	public String updateHandler(@ModelAttribute Address address, HttpSession session, Model model, Principal principal)
+	{
+		
+		try {
+			
+			User user = this.userRepository.getUserByUserName(principal.getName());
+			address.setUser(user);
+			
+			this.addressRepository.save(address);
+			
+			session.setAttribute("message", new Message("Your Address Is Updated", "success"));
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		System.out.println("Address" +address.getHouse_no());
+		
+		return "redirect:/user/" +address.getA_id()+"address";
+		
+	}
+	
 	
 }
